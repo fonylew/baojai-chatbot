11eea99055ce5c1195185f6144ebc8b47b81e543
Kamolphan Lewprasert <2770263+fonylew@users.noreply.github.com>
2024-03-08 11:56:55 +0700

    Fix import

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -58,11 +58,12 @@ async function run() {
 run();
 */
 
-const {
+import {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
-} = require("@google/generative-ai");
+} from "@google/generative-ai";
+
 
 const MODEL_NAME = "gemini-1.0-pro";
 const API_KEY = `${config.GEMINI_API_KEY}`;
